c2a75844881230cdcb77886ddf759a0bd563aa44
Valentina Angelova <vangelova89@gmail.com>
2022-05-11 10:41:22 -0500

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const {API_URL, PRIVATE_KEY} = process.env;
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
+};
